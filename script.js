cce90283222cb7fb435da9f3d9ddbc843c57b2c3
daraphista <daraphistadaone@gmail.com>
2021-12-04 21:21:17 +0800

    added popup form

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const gallery = document.querySelector('.card-gallery');
 const addButton = document.querySelector('.add');
 const menuButton = document.querySelector('.book');
 const screenBlur = document.querySelector('.blur');
+const popup = document.querySelector('.form-popup')
 const hiddenButtons = Array.from(document.querySelectorAll('.hidden'));
 const books = [];
 let counter = 1;
@@ -29,30 +30,38 @@ menuButton.addEventListener('click', () => {
     });
 });
 
-addButton.addEventListener('click', () => {
-  screenBlur.classList.toggle('active');
-
-  hiddenButtons.forEach(button => {
-    button.classList.add('hidden');
-  })
-
-  const currentDate = new Date();
-
-  currentCard = document.createElement('div');
-  currentCard.classList.add('card');
-  currentCard.dataset.index = counter;
-
-  gallery.appendChild(currentCard);
-  books[counter] = new Book('The Hobbit', 'J.R.R Tolkiens', '299', currentDate);
-  books[counter].index = document.querySelector(`div[data-index="${counter}"]`);
-  
-  for(const property in books[counter]) {
-    if(property != 'index') {
-      p = document.createElement('p');
-      p.textContent = `${property}: ${books[counter][property]}`;
-      books[counter].index.appendChild(p);
-    }
-  }
-
-  counter += 1;
+screenBlur.addEventListener('click', () => {
+  console.log('lmao');
 })
+
+addButton.addEventListener('click', () => {
+  popup.classList.toggle('active');
+});
+
+// addButton.addEventListener('click', () => {
+//   screenBlur.classList.toggle('active');
+
+//   hiddenButtons.forEach(button => {
+//     button.classList.add('hidden');
+//   })
+
+//   const currentDate = new Date();
+
+//   currentCard = document.createElement('div');
+//   currentCard.classList.add('card');
+//   currentCard.dataset.index = counter;
+
+//   gallery.appendChild(currentCard);
+//   books[counter] = new Book('The Hobbit', 'J.R.R Tolkiens', '299', currentDate);
+//   books[counter].index = document.querySelector(`div[data-index="${counter}"]`);
+  
+//   for(const property in books[counter]) {
+//     if(property != 'index') {
+//       p = document.createElement('p');
+//       p.textContent = `${property}: ${books[counter][property]}`;
+//       books[counter].index.appendChild(p);
+//     }
+//   }
+
+//   counter += 1;
+// })
