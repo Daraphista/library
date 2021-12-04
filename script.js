36a38abb7f502d9189923d2b64fd6a7f11125a33
daraphista <daraphistadaone@gmail.com>
2021-12-04 12:33:53 +0800

    cards can now display object properties

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
 const gallery = document.querySelector('.card-gallery');
 const addButton = document.querySelector('.add-button');
 const books = [];
-const counter = 1;
+let counter = 1;
 let h3;
-let card;
+let currentCard;
 
-function Book(title, author, pages, status, index) {
+function Card(index) {
+  this.index = index;
+}
+
+Book.prototype = Object.create(Card.prototype);
+
+function Book(title, author, pages, currentDate) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this['date added'] = `${currentDate.getUTCDate()}/${currentDate.getUTCMonth()}/${currentDate.getUTCFullYear()}`;
+  // console.log(this.date.getUTCDate());
+  // console.log(this.date.getUTCMonth()+1);
+  // console.log(this.date.getUTCFullYear());
 }
 
 addButton.addEventListener('click', () => {
-  card = document.createElement('div');
-  card.classList.add('card');
-  card.dataset.index = counter;
+  const currentDate = new Date();
 
-  gallery.appendChild(card);
-  books[counter] = new Book('The Hobbit', 'J.R.R Tolkiens', '299');
+  currentCard = document.createElement('div');
+  currentCard.classList.add('card');
+  currentCard.dataset.index = counter;
+
+  gallery.appendChild(currentCard);
+  books[counter] = new Book('The Hobbit', 'J.R.R Tolkiens', '299', currentDate);
   books[counter].index = document.querySelector(`div[data-index="${counter}"]`);
   
   for(const property in books[counter]) {
-    p = document.createElement('p');
-    p.textContent = `${property}: ${books[counter][property]}`;
-    books[counter].index.appendChild(p);
+    if(property != 'index') {
+      p = document.createElement('p');
+      p.textContent = `${property}: ${books[counter][property]}`;
+      books[counter].index.appendChild(p);
+    }
   }
+
+  counter += 1;
 })
 
 // books[0] = new Book('the Hobbit', 'J.R.R Tolkiens');
